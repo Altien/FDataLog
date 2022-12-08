a1604733fdd2ed67de1100e2f2f15a22d0d87ce0
Ambika Eshwar <ambika@rosalogia.me>
2022-12-08 13:28:59 -0600

    Add executable to bottom_up_cli for testing the parser

---
+++ src/bottom_up_cli/Program.fs
@@ -0,0 +1,17 @@
+﻿open System.IO
+open FSharp.Text.Lexing
+
+let testLexerAndParserFromFile (fileName:string) = 
+    use textReader = new System.IO.StreamReader(fileName)
+    let lexbuf = LexBuffer<char>.FromTextReader textReader
+
+    let countFromParser = Parser.parse_file Lexer.token lexbuf
+
+    printfn "countFromParser: result = %A" countFromParser
+
+
+let testFile = Path.Combine(__SOURCE_DIRECTORY__, "test.txt")
+testLexerAndParserFromFile testFile
+
+printfn "Press any key to continue..."
+System.Console.ReadLine() |> ignore
